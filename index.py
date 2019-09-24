abd370fca22f4c79e7f219ce72691ce0eceaa41a
xiesiyang <673975231@qq.com>
2019-09-24 20:15:47 +0800

    change chinese to english

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ file_path = os.getenv("FILE_PATH")
 ifttt_event_name = os.getenv("IFTTT_EVENT_NAME")
 
 if ip_url is None or ifttt_maker_key is None or file_path is None or ifttt_event_name is None:
-    raise BaseException('请检查环境变量设置 GET_IP_URL , IFTTT_MAKER_KEY ,FILE_PATH,IFTTT_EVENT_NAME')
+    raise BaseException('please check environment  GET_IP_URL , IFTTT_MAKER_KEY ,FILE_PATH,IFTTT_EVENT_NAME')
 
 f = open(file_path, mode='r')
 info = json.load(f)
@@ -25,15 +25,15 @@ print('method:'+method)
 print('password:'+password)
 print('port:'+port)
 
-print('开始准备获取真实ip')
+print('prepare to get out ip')
 ip_req = request.urlopen(ip_url)
 ip = ip_req.read().decode()
 print('ip:'+ip)
 
 origin_info = method + ':' + password + '@' + ip + ':' + port
-print('原始ss信息:'+origin_info)
+print('origin ss info:'+origin_info)
 _info = 'ss://' + str(base64.b64encode(origin_info.encode('utf-8')), 'utf-8').replace('-', '+').replace("_", '/')
-print('编码后的ss信息'+_info)
+print('ss url '+_info)
 
 
 
@@ -43,7 +43,7 @@ data_json = parse.urlencode({
 ifttt_maker_url = "https://maker.ifttt.com/trigger/"+ifttt_event_name+"/with/key/"+ifttt_maker_key
 req = request.Request(ifttt_maker_url)
 req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-print('准备发送请求通知')
+print('prepare to send msg ')
 with request.urlopen(req, data=data_json.encode('utf-8')) as f:
     print('Status:', f.status, f.reason)
     for k, v in f.getheaders():
